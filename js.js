099af9291d3b7ba65adc1b8069463f3a2a59a434
Junwei <junwei.he.99@gmail.com>
2022-05-06 14:48:05 -0700

    Completed html, css layout of calculator

---
+++ js.js
@@ -0,0 +1,36 @@
+const add = function(num1, num2) {
+	return num1+num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1-num2;
+};
+
+const multiply = function(num1, num2) {
+  return num1*num2;
+};
+
+const divide = function(num1,num2){
+    return num1/num2;
+};
+
+const operate = function(operator, num1, num2)
+{
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+            break;
+        case "-":
+            return subtract(num1,num2);
+            break;
+        case "*":
+            return multiply(num1,num2);
+            break;
+        case "/":
+            return divide(num1,num2);
+            break;
+        default:
+            return "invalid operator";
+    }
+};
+
