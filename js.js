d91a26a4e365b8831acf015624b32d8ea95293ca
Junwei <junwei.he.99@gmail.com>
2022-05-07 02:21:25 -0700

    Implemented a working calculator that displays results, but does not yet support signs, and percentage

--- js.js
+++ js.js
@@ -34,3 +34,105 @@ const operate = function(operator, num1, num2)
     }
 };
 
+const display = document.querySelector('.results');
+display.textContent = 0;
+
+let firstOperand = 0;
+let secondOperand = 0;
+let operation = "";
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(display.textContent == 0 || display.textContent == firstOperand)
+        {
+            display.textContent = "";
+        }
+        switch(button.textContent){
+            case "0":
+                display.textContent +=0;
+                break;
+            case "1":
+                display.textContent +=1;
+                break;
+            case "2":
+                display.textContent +=2;
+                break;
+            case "3":
+                display.textContent +=3;
+                break;
+            case "4":
+                display.textContent +=4;
+                break;
+            case "5":
+                display.textContent +=5;
+                break;
+            case "6":
+                display.textContent +=6;
+                break;
+            case "7":
+                display.textContent +=7;
+                break;
+            case "8":
+                display.textContent +=8;
+                break;
+            case "9":
+                display.textContent +=9;
+                break;
+            case ".":
+                display.textContent +=".";
+                break;
+            default:
+                break;
+        }
+    });
+});
+
+const operations = document.querySelectorAll('.operator');
+operations.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(firstOperand !== 0)
+        {
+            secondOperand = display.textContent;
+            if(operation == "+")
+            {
+               firstOperand = operate(operation, firstOperand, secondOperand);
+            }
+            else
+            {
+                firstOperand = parseFloat(operate(operation, firstOperand, secondOperand).toFixed(4));
+            }
+            display.textContent = firstOperand;
+            operation = button.textContent;
+            secondOperand =0;
+        }
+        else
+        {
+            operation = button.textContent;
+            firstOperand = display.textContent;
+        }
+    });
+});
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => {
+    secondOperand = display.textContent;
+    if(operation == "+")
+    {
+        display.textContent = operate(operation, firstOperand, secondOperand);
+    }
+    else
+    {
+        display.textContent = parseFloat(operate(operation, firstOperand, secondOperand).toFixed(4));
+    }
+    firstOperand = display.textContent;
+    operation = "";
+    secondOperand = 0;
+});
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    firstOperand = secondOperand = 0;
+    operation = "";
+    display.textContent = 0;
+});
